b4b7a359c0cecff31d0acc06c7f60a68bbc3079f
FelipeKatao <cataofelipe@gmail.com>
2023-11-14 12:00:41 -0300

    Data analitic

--- repository/DataAnalitc_Repository.py
+++ repository/DataAnalitc_Repository.py
@@ -3,7 +3,14 @@ import pymysql
 class DataAnalitic():
     def __init__(self) -> None:
         self.con = pymysql.connect(host="127.0.0.1",user="root",passwd="alunolab",database="folhadepagamento")
+        
+    def AtualizarBanco(self):
+        self.con.close()
+        self.con = pymysql.connect(host="127.0.0.1",user="root",passwd="alunolab",database="folhadepagamento")
+        
     def AcessarBanco(self,NomeTabela):
+            
+        self.AtualizarBanco()         
         sql = f"""
           SELECT * FROM {NomeTabela} 
           """
@@ -12,6 +19,6 @@ class DataAnalitic():
             cursor.execute(sql)
             result = cursor.fetchall()
             return list(result)
-        except:
-            return "Ocorreu um erro no processamento"
+        except  Exception as e :
+            return "Ocorreu um erro no processamento "+str(e)
         
