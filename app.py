6d8a62fb9e2d57a5a904d936d03351b5ff1cccc1
FelipeKatao <cataofelipe@gmail.com>
2023-11-21 09:41:54 -0300

    update

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask,request,session
+import logging
+from flask import Flask,request,session,Response
 from view.data_views import DataRoutes
 from controler.data_route import Controler_dadosBrutos
 
@@ -11,19 +12,26 @@ app.add_url_rule("/dados","ListagemDados",DadosBrutos.ListagemDados)
 
 @app.route("/login")
 def loginBase():
-    session['username'] = 'DadosMestre'
+    auth = request.authorization
+    app.logger.info("logando...")
+    if(auth.username != 'DadoMestre'):
+        return {'status':'Erro ao logar na conta',f'username':''+auth.get('username')+''}
+    
+    session['name'] = auth.username
+    
     return {"Status":"Logado com sucesso"}
 
 @app.route("/baseDados")
 def baseDados():
-    auth = request.authorization
-    if(auth and auth.username == "DadosMestre"):
+    if(session.get('name') == "DadosMestre"):
         return DadosBrutos.BaseDados()
     else:
-        {"status":"Login Requerido"}
-
+       return Response("{'a':'Erro de autorização'}", status=401)
 app.permanent_session_lifetime = 60 
 app.secret_key = "SECRET KEY"
+
+
+logging.basicConfig(filename='C:/Users/SNMACT145/Desktop/ApiAnaliseDados/Data/Dataserver.log')
 
 if(__name__ == '__main__'):
     app.run(debug=True)
--- static/js/main.js
+++ static/js/main.js
@@ -51,6 +51,33 @@ function PaginaSobre(){
     `,true)
 }
 
+function PaginaLogin(){
+    oct8.ModifyContentContainer(document.getElementById("Pagina_projeto"),`
+    <form>
+    <label>Nome </label><input id='input_user' type='text'></input>
+    <button id='conId'>Conectar</button>
+    <div class='aviso'></div>
+    </form>
+    `,true)
+    
+    document.getElementById('conId').addEventListener("click",()=>{
+        fetch("http://127.0.0.1:5000/login",{method:"GET",
+        headers: {'Authorization': 'Basic ' + btoa(document.getElementById('input_user').value+':'+'00')}})
+        .then(response =>response.json(),Headers)
+        .then(json =>{
+            let valueUser = document.getElementById('input_user').value
+            console.log(json)
+            if(json['Status'] != 'Logado com sucesso')
+            {
+                document.getElementsByClassName('aviso')[0].innerHTML="Falha ao conectar!"
+            }
+            else
+            {
+                window.location.href = "http://127.0.0.1:5000/#/dados"
+            }
+        })})
+}
+
 function NotFoundPage(){
     oct8.ModifyContentContainer(document.getElementById("Pagina_projeto"),`
     <h1>404</h1>
@@ -61,5 +88,6 @@ function NotFoundPage(){
 
 oct8.CreateNewRoute("/sobre",PaginaSobre)
 oct8.CreateNewRoute("/dados",dadosPagina)
+oct8.CreateNewRoute("/login",PaginaLogin)
 oct8.AddNotFoundPage(PaginaPrincipal)
 oct8.LoadRoutes()
