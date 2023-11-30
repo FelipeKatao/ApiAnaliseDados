c74850691f426a98b0ebdbdfcbb8206b7c71c25f
FelipeKatao <cataofelipe@gmail.com>
2023-11-30 11:05:52 -0300

    update

--- app.py
+++ app.py
@@ -15,10 +15,12 @@ app.add_url_rule("/dados","ListagemDados",DadosBrutos.ListagemDados)
 
 @app.route("/teste")
 def testeRota():
+    pymysql.Connect.connect_timeout = 5000
     con = pymysql.connect(host='db4free.net',user='usuaro_0_poli',password='9090ola1',port=3306,charset='utf8')
+
     return 'None'
 
-logging.basicConfig(filename='C:/Users/SNMACT145/Desktop/ApiAnaliseDados/logs/Dataserver.log')
+#logging.basicConfig(filename='C:/Users/SNMACT145/Desktop/ApiAnaliseDados/logs/Dataserver.log')
 
 app.secret_key = "SECRET KEY"
 app.permanent_session_lifetime = 60
