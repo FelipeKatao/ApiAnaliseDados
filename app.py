117f793ce980a5eb6086ab5525461f1f0acd03d6
FelipeKatao <cataofelipe@gmail.com>
2023-12-05 11:25:59 -0300

    Sistema de upload

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 import logging
-from flask import Flask
+import io
+import os
+from flask import Flask,request
+import requests
+from flask_cors import CORS
 from routes.login_route import Login_route
 from view.data_views import DataRoutes
 from controler.data_route import Controler_dadosBrutos
+from io import BytesIO
 import pymysql
 
 app = Flask(__name__)
@@ -10,15 +15,21 @@ DataRoute_= DataRoutes()
 DadosBrutos = Controler_dadosBrutos()
 app.register_blueprint(Login_route)
 
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
 app.add_url_rule("/","ListarProdutos",DataRoute_.ListarProdutos,methods=['GET','POST'])
 app.add_url_rule("/dados","ListagemDados",DadosBrutos.ListagemDados) 
 
-@app.route("/teste")
-def testeRota():
-    pymysql.Connect.connect_timeout = 5000
-    con = pymysql.connect(host='db4free.net',user='usuaro_0_poli',password='9090ola1',port=3306,charset='utf8')
-
-    return 'None'
+@app.post("/upload")
+def Upload():
+    let = request.get_data()
+    input_file = request.files['file'].read()
+    f = BytesIO(input_file)
+    with open("Nova_Imagem.png", 'wb') as f: 
+        f.write(input_file)
+    print(input_file)
+    return "file"
 
 #logging.basicConfig(filename='C:/Users/SNMACT145/Desktop/ApiAnaliseDados/logs/Dataserver.log')
 
